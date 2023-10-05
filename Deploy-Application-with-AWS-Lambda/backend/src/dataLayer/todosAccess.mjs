93ea20a4bc15ab954920036aad7b094e8d462f78
Oriental Tran <orientalinformation@gmail.com>
2023-10-05 23:04:25 +0700

    Fix S3

--- Deploy-Application-with-AWS-Lambda/backend/src/dataLayer/todosAccess.mjs
+++ Deploy-Application-with-AWS-Lambda/backend/src/dataLayer/todosAccess.mjs
@@ -2,7 +2,7 @@ import { DynamoDB } from '@aws-sdk/client-dynamodb'
 import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb'
 import { createLogger } from '../utils/logger.mjs'
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+//import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 
 // TODO: Implement dataLayer
 const logger = createLogger("TodoAccess");
@@ -142,7 +142,7 @@ export class TodosAccess {
 
             // return url
 
-            const uploadUrl = await getSignedUrl("putObject", {
+            const uploadUrl = this.S3.getSignedUrl("putObject", {
                 Bucket: this.bucket_name,
                 Key: todoId,
                 Expires: parseInt(url_expiration)
